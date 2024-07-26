abb4524b29e012dc6149035806d9984695764c23
pikac <pikac@91act.com>
2024-07-26 17:48:56 +0800

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -23,7 +23,7 @@ public class Menu extends GameCanvas implements Runnable {
     private int exit_x;
     private int exit_y;
 
-    private String version="V1.2";
+    private String version="V1.3";
 
     public Menu() {
         super(false);
